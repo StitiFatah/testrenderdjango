e9b68a0ce52fce49723affe9832e23ad39265617
D_user <ffoot9698@gmail.com>
2021-12-27 16:27:56 +0100

    new3

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit
 pip install -r requirements.txt 
 
 python manage.py collectstatic --no-input
+
+python manage.py makemigrations users
+python mange.py makemigrations 
 python manage.py migrate
