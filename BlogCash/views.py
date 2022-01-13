9c24a3e98cfe582b86490b4d2e3260faf8143f9a
D_user <ffoot9698@gmail.com>
2022-01-13 16:43:57 +0100

    test domain

--- BlogCash/views.py
+++ BlogCash/views.py
@@ -5,6 +5,9 @@ from .serializers import TestImagesSerializer
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import permissions
 
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+from rest_framework import status
 
 def index(request):
     return HttpResponse("Hello guys")
@@ -24,3 +27,23 @@ class TestImageGet(RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = TestImagesSerializer
     queryset = TestImages.objects.all()
+
+class TestDomain(APIView):
+
+    def get(self, request, *args, **kwargs):
+
+        # resp = str(request.META)
+        # resp = "hello"
+
+        resp = {
+            "http_host": request.META["HTTP_HOST"],
+            "http_origin": request.META["HTTP_ORIGIN"],
+        }
+
+        resp = {
+            "meta": str(request.META)
+        }
+
+        return Response(resp, status=status.HTTP_200_OK)
+
+
