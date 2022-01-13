246eec1fe95a72257f6e2a443310e5459020e533
D_user <ffoot9698@gmail.com>
2022-01-13 17:00:53 +0100

    test domain 2

--- BlogCash/urls.py
+++ BlogCash/urls.py
@@ -1,21 +1,6 @@
-"""BlogCash URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, indexdb, TestImagesList, TestImageGet
+from .views import index, indexdb, TestImagesList, TestImageGet,TestDomain
 from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
@@ -35,8 +20,9 @@ urlpatterns = [
     ###
     path("list_test_images/", TestImagesList.as_view(), name="test-image-list"),
     path("get_test_image/<int:pk>/", TestImageGet.as_view(), name="test-image-get"),
-
-]
+    
+    path("test_domain/", TestDomain.as_view(), name="test-domain")
+    ]
 
 if settings.DEBUG is True:
     urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
